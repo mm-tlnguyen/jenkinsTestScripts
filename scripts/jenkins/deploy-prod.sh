b1849e422408db0d043bad9e1806137326a7c92b
Tung Lam Nguyen <33957675+mm-tlnguyen@users.noreply.github.com>
2019-08-22 16:20:21 +0300

    Update deploy-prod.sh

--- scripts/jenkins/deploy-prod.sh
+++ scripts/jenkins/deploy-prod.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-bash ./scripts/jenkins/deploy.sh '1.2.3.4' 'prod' 
+bash ./scripts/jenkins/deploy.sh '1.2.3.4' 'prod'
+
+sleep 5
+
 bash ./scripts/jenkins/deploy.sh '5.6.7.8' 'prod' true
